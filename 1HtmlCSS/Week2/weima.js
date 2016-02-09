6c02aea84b3aec5926ad48af6d843dd95f563ffd
cutemelon <cutemelon21@gmail.com>
2016-02-09 03:39:35 -0600

    course 1 - html css and javascript
    basics

---
+++ 1HtmlCSS/Week2/weima.js
@@ -0,0 +1,44 @@
+var user_name; // Create a variable
+user_name=prompt("What is your name?");
+
+
+var done = false;
+while(!finished){
+	response = prompt();
+	if(condition){ //contains the color
+		finished = true;
+	}
+}
+
+var guess_count = 0;
+var game_guess;
+var user_guess;
+var questionStr = "I am thinking of one of these colors:\n\n" + 
+"blue, cyan, gold, gray, green, magenta, orange, red, white, yellow\n\n"+
+"What color am I thinking of?\n"
+
+var colors = ["blue"", "cyan", "gold", "gray", "green", "magenta", "orange", "red", "white", "yellow"]
+user_guess=prompt(questionStr);
+
+if(colors do not contain user_guess){
+	gues_count ++;
+	alert();
+}else if (user_guess > game_guess){
+	alert();
+}else if(user_guess < game_guess){
+	alert();
+}else{
+	print count;
+	print success;
+}
+
+
+<script>
+function askQuestion() {
+}
+</script>
+
+<body onload = "askQuestion()";>
+function askQuestion() {
+	//prompt
+}
